8d78185e47d1279fa1f0c52115d945056ff270be
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-01-02 11:03:33 -0600

    Use subscripts to poke into array

--- src/netnames.c
+++ src/netnames.c
@@ -2,23 +2,22 @@
 #include "dict.h"
 #include "tcp_states.h"
 
-static dict_entry_t const	names[13] =	{
-	[0              ] = "0",
-	[TCP_CLOSE      ] = "TCP_CLOSE",
-	[TCP_CLOSE_WAIT ] = "TCP_CLOSE_WAIT",
-	[TCP_CLOSING    ] = "TCP_CLOSING",
-	[TCP_ESTABLISHED] = "TCP_ESTABLISHED",
-	[TCP_FIN_WAIT1  ] = "TCP_FIN_WAIT1",
-	[TCP_FIN_WAIT2  ] = "TCP_FIN_WAIT2",
-	[TCP_LAST_ACK   ] = "TCP_LAST_ACK",
-	[TCP_LISTEN     ] = "TCP_LISTEN",
-	[TCP_MAX_STATES ] = "TCP_MAX_STATES",
-	[TCP_SYN_RECV   ] = "TCP_SYN_RECV",
-	[TCP_SYN_SENT   ] = "TCP_SYN_SENT",
-	[TCP_TIME_WAIT  ] = "TCP_TIME_WAIT",
+static dict_entry_t const	names[] =	{
+	[TCP_ESTABLISHED]   = {    "TCP_ESTABLISHED", "ESTABLISHED" },
+	[TCP_SYN_SENT]      = {       "TCP_SYN_SENT", "SYN_SENT" },
+	[TCP_SYN_RECV]      = {       "TCP_SYN_RECV", "SYN_RECV" },
+	[TCP_FIN_WAIT1]     = {      "TCP_FIN_WAIT1", "FIN_WAIT1" },
+	[TCP_FIN_WAIT2]     = {      "TCP_FIN_WAIT2", "FIN_WAIT2" },
+	[TCP_TIME_WAIT]     = {      "TCP_TIME_WAIT", "TIME_WAIT" },
+	[TCP_CLOSE]         = {          "TCP_CLOSE", "CLOSE" },
+	[TCP_CLOSE_WAIT]    = {     "TCP_CLOSE_WAIT", "CLOSE_WAIT" },
+	[TCP_LAST_ACK]      = {       "TCP_LAST_ACK", "LAST_ACK" },
+	[TCP_LISTEN]        = {         "TCP_LISTEN", "LISTEN" },
+	[TCP_CLOSING]       = {        "TCP_CLOSING", "CLOSING" },
+	[TCP_MAX_STATES]    = {     "TCP_MAX_STATES", "MAX_STATES" },
 };
 
 dict_t	const	netdict =	{
 	names,
-	13
+	sizeof(names)/sizeof(names[0])
 };
