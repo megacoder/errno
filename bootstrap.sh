d5730e9944f2e5f72426437dba5aba5ac2632a55
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2014-02-24 08:09:55 -0600

    Local directory RPM build

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 autoreconf -fvim
+./configure
+make dist
+rm -rf RPM
+mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+rpmbuild                                                                \
+        -D"_topdir ${PWD}/RPM"                                          \
+        -D"_sourcedir ${PWD}"                                           \
+        -D"_specdir ${PWD}"                                           \
+        -ba                                                             \
+        errno.spec
