9b91824d24aaa72f5c8e9b9e0accb4d547c5639e
Tommy Reynolds <Tommy.Reynolds@MegaCoder.com>
2016-09-09 15:16:34 -0500

    Automatically output a log mess.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-autoreconf -fvim
-./configure
-make dist
-rm -rf RPM
-mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
-rpmbuild                                                                \
-        -D"_topdir ${PWD}/RPM"                                          \
-        -D"_sourcedir ${PWD}"                                           \
-        -D"_specdir ${PWD}"                                           \
-        -ba                                                             \
-        errno.spec
+(
+	autoreconf -fvim
+	./configure
+	make dist
+	rm -rf RPM
+	mkdir -p RPM/{SOURCES,RPMS,SRPMS,BUILD,SPECS}
+	rpmbuild							\
+		-D"_topdir ${PWD}/RPM"					\
+		-D"_sourcedir ${PWD}"					\
+		-D"_specdir ${PWD}"					\
+		-ba							\
+		errno.spec
+) 2>&1 | tee bootstrap.log
